43740cd92c5f1b817693addffbef24b1112b2b74
Wain <wain-pc@mail.ru>
2016-07-26 01:00:34 +0300

    -Returned the style fix (acidentally removed it during the bug research)

--- ingfilm.js
+++ ingfilm.js
@@ -233,6 +233,10 @@ function locateMainPlayerLink(page, url, response) {
         if (iframeUrl && iframeUrl[1]) {
             iframeUrl = 'http://' + iframeUrl[1];
 
+            //Replace wrong URL param 'style' with the correct value (1)
+            //This is a possible fix for Issue #2
+            iframeUrl = iframeUrl.replace(/style=\d+?/, 'style=1');
+
             //Step 3. Load Iframe content from URL
             iframeResponse = makeRequest(page, iframeUrl, null, true);
 
